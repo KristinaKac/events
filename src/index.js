49156b8083ff1580c03cc5f40d16163d097c859a
Kristina <kachalkovakristina@mail.ru>
2023-12-29 19:03:23 +0300

    3.0.2

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './css/style.css';
-import GameController from './js/GameController';
-import PlayingField from './js/PlayingField';
-
-const playingField = new PlayingField(4);
-
-// eslint-disable-next-line
-const gameController = new GameController(playingField);
